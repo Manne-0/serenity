d9d7945a2eb60e03c713eae24eab46a4fb1f0731
Chidinma Okoro <okoromannie@gmail.com>
2024-11-29 10:48:03 +0000

    static files settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blog", "static"),  # For project-level static files
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
