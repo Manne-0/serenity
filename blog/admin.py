7e849315e1ce1e9456a2bd6c8a89d759dd5238c5
Chidinma Okoro <okoromannie@gmail.com>
2024-12-18 10:25:50 +0000

    UI Update

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_created', 'summary', 'image')
-    search_fields = ('title', 'content')
+    list_display = ('title', 'date_created', 'summary', 'image', 'song_title')  # Show song_title in the list view
+    search_fields = ('title', 'content', 'song_title')  # Allow searching by song_title
     list_filter = ('date_created',)
+    fields = ('title', 'content', 'summary', 'image', 'youtube_link', 'song_title', 'song_message')  # Add fields for the form
 
 # Register the customized admin
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,11 @@ class Post(models.Model):
     summary = models.TextField(default="Default summary")
     date_created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True) 
+    youtube_link = models.URLField(blank=True, null =True)
+    song_title = models.CharField(max_length=200, null=True, blank=True)  # Song title and artist
+    song_message = models.TextField(null=True, blank=True)  # Custom message for the song section
+
+
 
 
     def __str__(self):
